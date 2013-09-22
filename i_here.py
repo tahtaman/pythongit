fbaa846ca0476a926d52636de75470fbfaec1a94
tahtaman <tahtaman@gmail.com>
2013-09-22 15:46:58 +0400

    add function to i_here

--- i_here.py
+++ i_here.py
@@ -1,45 +1,4 @@
-print('i here')
-" ============================================================================
-" Netrw Directory Listing                                        (netrw v143)
-"   /home/tahtaman/codeschool/python
-"   Sorted by      name
-"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
-"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec
-" ============================================================================
-../
-.ropeproject/
-__pycache__/
-test.py*
-atm.py
-darkside.vim
-find_word.py
-kitty.txt
-maker.py
-myau.py
-myfile.txt
-name.py
-speed.py
-style.css
-.atm.py.swp
-i ============================================================================
-" Netrw Directory Listing                                        (netrw v143)
-"   /home/tahtaman/codeschool/python
-"   Sorted by      name
-"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
-"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec
-" ============================================================================
-../
-.ropeproject/
-__pycache__/
-test.py*
-atm.py
-darkside.vim
-find_word.py
-kitty.txt
-maker.py
-myau.py
-myfile.txt
-name.py
-speed.py
-style.css
-.atm.py.swp
+def i_here(location):
+    print('I here ->', location)
+
+i_here('London')
