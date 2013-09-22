63ccc3f90cb129ef74c49b2716eaf087b57f3286
tahtaman <tahtaman@gmail.com>
2013-09-22 16:26:05 +0400

    upgrade func i_here - add sys.argv

--- i_here.py
+++ i_here.py
@@ -1,4 +1,9 @@
+import sys
+
+name_argument = sys.argv[1]
+location_argument = sys.argv[2]
+
 def i_here(name, location):
     print('My name is {0}. I am in {1}'.format(name, location))
 
-i_here('Martin', 'London')
+i_here(name_argument, location_argument)
